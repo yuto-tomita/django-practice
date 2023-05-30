1a3730bb1e0e8c4ec498a1d31ead559c0ebdacf7
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2023-05-30 23:47:29 +0900

    timezoneの設定

--- modules/modules/settings.py
+++ modules/modules/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todos',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -109,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
