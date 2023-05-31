fc445574adf50879cebde0df4765c58527b15eb2
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2023-05-31 22:33:59 +0900

    refactor: ディレクトリ間の移動

--- modules/modules/settings.py
+++ modules/modules/settings.py
@@ -31,14 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'todos',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todos',
-    'users'
 ]
 
 MIDDLEWARE = [
