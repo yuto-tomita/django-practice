1039f66c47880c26be533f06623bd43c7b6cb7d2
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2023-05-31 22:44:29 +0900

    fix: モデルの記述内容修正

--- modules/todos/models.py
+++ modules/todos/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from users.models import User
 from django.utils import timezone
 
 # django では `primary_key=True` を設定しない場合、自動的に主キーを設定するための IntegerField を追加する
 # https://docs.djangoproject.com/ja/4.2/topics/db/models/#automatic-primary-key-fields
 class Todo(models.Model):
-  user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todos")
   title = models.CharField(max_length=200)
   description = models.CharField(default="")
   date = models.DateField(default=timezone.now)
