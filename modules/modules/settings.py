268adbb287a129245b90caf16330db5bc2c6fb56
yuto-tomita <65475371+yuto-tomita@users.noreply.github.com>
2023-05-31 22:54:53 +0900

    fix: カスタムユーザークラスを使用するために既存のadmin クラスを installd apps から除外

--- modules/modules/settings.py
+++ modules/modules/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'todos',
     'users',
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
